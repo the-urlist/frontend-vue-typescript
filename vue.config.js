a2c03c4a6434d77afea3427122fdc6eed2e0e805
Burke Holland <burkeholland@gmail.com>
2019-11-01 11:33:36 -0500

    Update launch config for debugging

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,7 @@ module.exports = {
       }
     }
   },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === "development") {
-      config.devtool = "eval-source-map";
-      config.output.devtoolModuleFilenameTemplate = info =>
-        info.resourcePath.match(/\.vue$/) &&
-        !info.identifier.match(/type=script/) // this is change ✨
-          ? `webpack-generated:///${info.resourcePath}?${info.hash}`
-          : `webpack-yourCode:///${info.resourcePath}`;
-
-      config.output.devtoolFallbackModuleFilenameTemplate =
-        "webpack:///[resource-path]?[hash]";
-    }
+  configureWebpack: {
+    devtool: "source-map"
   }
 };

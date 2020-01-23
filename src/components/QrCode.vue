7ae02d8717fe42500a86a686c5a9319810b4558d
Burke Holland <burkeholland@gmail.com>
2020-01-23 08:21:55 -0600

    Add QR Code to List View

---
+++ src/components/QrCode.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <qrcode-vue
+      :value="value"
+      size="70%"
+      level="H"
+      background="#F9FAFC"
+      renderAs="svg"
+    ></qrcode-vue>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import QrcodeVue from "qrcode.vue";
+
+@Component({
+  components: {
+    QrcodeVue
+  },
+  props: {
+    value: String
+  }
+})
+export default class QrCode extends Vue {
+  data() {
+    return {
+      size: window.innerWidth
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/views/List.vue
+++ src/views/List.vue
@@ -6,39 +6,78 @@
           class="is-size-3 is-heading has-text-weight-medium has-text-primary"
         >
           {{ currentList.description }}
-          <div
-            id="share"
-            v-if="currentList.links.length > 0"
-            class="buttons has-addons"
-          >
-            <a
-              class="button"
-              target="_blank"
-              :href="
-                `https://twitter.com/intent/tweet?text=${currentList.description} theurlist.com/${currentList.vanityUrl}`
-              "
-              ><span class="icon"><i class="fab fa-twitter"></i></span
-            ></a>
-            <a
-              class="button"
-              target="_blank"
-              :href="
-                `https://www.facebook.com/sharer/sharer.php?u=theurlist.com/${currentList.vanityUrl}`
-              "
-              ><span class="icon"><i class="fab fa-facebook-f"></i></span
-            ></a>
-            <a
-              class="button"
-              target="_blank"
-              :href="
-                `https://www.linkedin.com/shareArticle?mini=true?summaary=${currentList.description}&url=https://theurlist.com?${currentList.vanityUrl}`
-              "
-              ><span class="icon"><i class="fab fa-linkedin-in"></i></span
-            ></a>
-          </div>
         </h2>
+        <div class="columns is-mobile">
+          <div class="column is-half">
+            <div
+              id="share"
+              v-if="currentList.links.length > 0"
+              class="buttons has-addons"
+            >
+              <a
+                class="button"
+                target="_blank"
+                :href="
+                  `https://twitter.com/intent/tweet?text=${currentList.description} theurlist.com/${currentList.vanityUrl}`
+                "
+              >
+                <span class="icon"><i class="fab fa-twitter"></i></span>
+              </a>
+              <a
+                class="button"
+                target="_blank"
+                :href="
+                  `https://www.facebook.com/sharer/sharer.php?u=theurlist.com/${currentList.vanityUrl}`
+                "
+              >
+                <span class="icon"><i class="fab fa-facebook-f"></i></span>
+              </a>
+              <a
+                class="button"
+                target="_blank"
+                :href="
+                  `https://www.linkedin.com/shareArticle?mini=true?summary=${currentList.description}&url=https://theurlist.com?${currentList.vanityUrl}`
+                "
+              >
+                <span class="icon"><i class="fab fa-linkedin-in"></i></span>
+              </a>
+            </div>
+          </div>
+          <div class="column is-half">
+            <div
+              id="view"
+              v-if="currentList.links.length > 0"
+              class="buttons has-addons is-pulled-right"
+            >
+              <button
+                title="View as List"
+                class="button"
+                :class="{ 'is-active': !qr }"
+                @click="toggleQr(false)"
+              >
+                <span class="icon"><i class="fas fa-list-ul"></i></span>
+              </button>
+
+              <button
+                title="View as QR Code"
+                class="button"
+                :class="{ 'is-active': qr }"
+                @click="toggleQr(true)"
+              >
+                <span class="icon"><i class="fas fa-qrcode"></i></span>
+              </button>
+            </div>
+          </div>
+        </div>
         <br />
-        <link-list :links="currentList.links" :editable="false"></link-list>
+        <link-list
+          :links="currentList.links"
+          :editable="false"
+          v-if="!qr"
+        ></link-list>
+        <div class="has-text-centered" v-else>
+          <qr-code :value="`https://theurlist.com/${this.vanityUrl}`"></qr-code>
+        </div>
       </div>
       <div v-if="vanityUrl.length > 0">
         <not-found
@@ -55,16 +94,19 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 import LinkList from "@/components/LinkList.vue";
 import NotFound from "@/components/NotFound.vue";
+import QrCode from "@/components/QrCode.vue";
 
 @Component({
   components: {
     LinkList,
-    NotFound
+    NotFound,
+    QrCode
   }
 })
 export default class List extends Vue {
   notFound: boolean = false;
   vanityUrl: string = "";
+  qr: boolean = false;
 
   get currentList() {
     return this.$store.getters.currentList;
@@ -85,6 +127,10 @@ export default class List extends Vue {
     } catch (err) {
       this.notFound = true;
     }
+  }
+
+  toggleQr(flag: boolean) {
+    this.qr = flag;
   }
 }
 </script>

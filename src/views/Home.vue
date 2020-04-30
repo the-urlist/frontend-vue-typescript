a7ef7ad97dea35481bd116d71b28e9d2e9213073
Burke Holland <burkeholland@gmail.com>
2020-04-30 11:27:40 -0500

    Remove site description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,17 +8,9 @@
               <h1 class="is-size-3 has-text-weight-medium">
                 <span class="has-text-primary">Group</span> links,
                 <span class="has-text-primary">Save</span> &
-                <span class="has-text-primary">Share</span> them with the world!!!
+                <span class="has-text-primary">Share</span> them with the
+                world!!!
               </h1>
-              <br />
-              <p>
-                Add links to a list and share it with one simple URL
-              </p>
-              <br />
-              <p>
-                Create a list anonymously, or login to save, manage and edit
-                your lists
-              </p>
             </div>
           </div>
           <div class="column is-half is-hidden-mobile">
@@ -49,8 +41,8 @@ import NewLink from "@/components/NewLink.vue";
 
 @Component({
   components: {
-    NewLink
-  }
+    NewLink,
+  },
 })
 export default class Home extends Vue {
   // event emitted by the AddBar component

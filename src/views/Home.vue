e18a41b93ff996b6fe294403ac96b08dcfe75293
sana-ajani <sajani6@gatech.edu>
2020-05-01 14:43:04 -0400

    Add descriptiontion `Home.vue` Fixes #27 add instructions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,13 @@
                 <span class="has-text-primary">Share</span> them with the world
               </h1>
               <br />
-              // TODO: #27 @sana-ajani Add site description
+              <br />
+              <p>Add links to a list and share it with one simple URL.</p>
+              <br />
+              <p>
+                Create a list anonymously or login to save, manage and edit your
+                lists.
+              </p>
               <p>
                 Add links to a list and share it with one simple URL.
               </p>
@@ -50,8 +56,8 @@ import NewLink from "@/components/NewLink.vue";
 
 @Component({
   components: {
-    NewLink
-  }
+    NewLink,
+  },
 })
 export default class Home extends Vue {
   // event emitted by the AddBar component

ed41c69a387ef70754a4a1c823bceffb16564c4e
Burke Holland <burkeholland@gmail.com>
2020-05-01 11:42:11 -0500

    Remove description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,15 +10,6 @@
                 <span class="has-text-primary">Save</span> &
                 <span class="has-text-primary">Share</span> them with the world
               </h1>
-              <br />
-              <p>
-                Add links to a list and share it with one simple URL
-              </p>
-              <br />
-              <p>
-                Create a list anonymously, or login to save, manage and edit
-                your lists
-              </p>
             </div>
           </div>
           <div class="column is-half is-hidden-mobile">

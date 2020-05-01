28ce327f20285e431f8bff9fd0e29686497fe0ce
Burke Holland <burkeholland@gmail.com>
2020-05-01 11:25:02 -0500

    Tag @sana-ajani for Home Page Changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,7 @@
                 <span class="has-text-primary">Save</span> &
                 <span class="has-text-primary">Share</span> them with the world
               </h1>
+              // TODO: #21 @sana-ajani Add a description here
             </div>
           </div>
           <div class="column is-half is-hidden-mobile">

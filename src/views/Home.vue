13114185febf2bd62403fb5c3d560b182c81cdf3
sana-ajani <sajani6@gatech.edu>
2020-05-03 13:55:02 -0400

    Add a description Fixes #31

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,13 @@
                 <span class="has-text-primary">Save</span> &
                 <span class="has-text-primary">Share</span> them with the world
               </h1>
-              // TODO #31 Add a description please @sana-ajani
+              <br />
+              <p>Add links to a list and share it with one simple URL.</p>
+              <br />
+              <p>
+                Create a list anonymously or login to save, manage, and edit
+                your lists.
+              </p>
             </div>
           </div>
           <div class="column is-half is-hidden-mobile">
@@ -41,8 +47,8 @@ import NewLink from "@/components/NewLink.vue";
 
 @Component({
   components: {
-    NewLink
-  }
+    NewLink,
+  },
 })
 export default class Home extends Vue {
   // event emitted by the AddBar component
